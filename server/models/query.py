2fe9f044f18a7a341995d2ce8e4857e6ae478e55
LuChang-CS <2389156163@qq.com>
2019-12-03 16:29:13 -0500

    fix query bugs

--- server/models/query.py
+++ server/models/query.py
@@ -19,6 +19,7 @@ class Query:
             for page in page_list:
                 nid, score = page.page_id(), page.score()
                 page_set.add(nid)
+                page_dict[nid] = score
             if result_set is not None:
                 result_set = result_set.intersection(page_set)
                 if len(result_set) == 0:
@@ -27,6 +28,7 @@ class Query:
                     page_score[nid] = page_dict[nid] + page_score.get(nid, 0)
             else:
                 result_set = page_set
+                page_score = page_dict
         if result_set is None:
             return None
         return sorted([(nid, page_score[nid]) for nid in result_set],
