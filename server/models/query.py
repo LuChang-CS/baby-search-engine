8e2808ebbacde13b6f6168ded5d8c2d433d6f1fe
LuChang-CS <2389156163@qq.com>
2019-12-03 13:49:14 -0500

    optimize query

--- server/models/query.py
+++ server/models/query.py
@@ -15,15 +15,16 @@ class Query:
                 return None
             page_list = page_list.load(settings.WORDS_DIR)
             page_set = set()
+            page_dict = dict()
             for page in page_list:
                 nid, score = page.page_id(), page.score()
                 page_set.add(nid)
-                page_score[nid] = score + page_score.get(nid, 0)
-            print(word, page_set)
             if result_set is not None:
                 result_set = result_set.intersection(page_set)
                 if len(result_set) == 0:
                     return None
+                for nid in result_set:
+                    page_score[nid] = page_dict[nid] + page_score.get(nid, 0)
             else:
                 result_set = page_set
         if result_set is None:
